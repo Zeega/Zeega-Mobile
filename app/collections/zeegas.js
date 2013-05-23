e733afa3aceb44fdd2c1ba718094e63799f25d29
James Burns <jameburn@gmail.com>
2013-05-23 14:45:26 -0700

    reduce home background image size and number of zeegas returned

--- app/collections/zeegas.js
+++ app/collections/zeegas.js
@@ -6,10 +6,10 @@ module.exports = Base.extend({
   url: function() {
       
     if ( _.isUndefined(this.params.tags) ) {
-        url = '/api/items/search?type=project&limit=10&fields=id,title,display_name,user_id,views,user_thumbnail,thumbnail_url&sort=date-desc&tags=homepage';
+        url = '/api/items/search?type=project&limit=5&fields=id,title,display_name,user_id,views,user_thumbnail,thumbnail_url&sort=date-desc&tags=homepage';
 
     } else {
-        url = '/api/items/search?user=:user&type=project&limit=20&fields=id,title,views,display_name,user_id,user_thumbnail,thumbnail_url&sort=date-desc';
+        url = '/api/items/search?user=:user&type=project&limit=10&fields=id,title,views,display_name,user_id,user_thumbnail,thumbnail_url&sort=date-desc';
     }
 
 
